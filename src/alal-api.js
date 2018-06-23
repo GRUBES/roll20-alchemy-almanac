64c33eabecb4f62d2dc24257a717b6260b665847
GRUBES <erictgrubaugh@gmail.com>
2018-06-23 11:59:24 -0600

    Refactored to leverage API Router common module

--- src/alal-api.js
+++ src/alal-api.js
@@ -9,98 +9,18 @@
  */
 
 import { version } from "../package.json";
+
 import * as AlchemyUtils from "./alal-util";
 import * as AlchemyGatherer from "./alal-gather";
 import * as AlchemyHarvester from "./alal-harvest";
 
-/**
- * Handler method for chat messages
- *
- * @param msg {Object} Roll20 chat message data
- *
- * @returns {void}
- *
- * @private
- * @function route
- */
-function route(msg) {
-    if (!isCommand(msg)) { return; }
-
-    execute(parseCommand(msg), parseInput(msg));
-}
-
-/**
- * Determines whether the given chat message is an Alchemy command
- *
- * @param msg {Object} Roll20 chat message data
- *
- * @returns {Boolean} true if the message is a Alchemy command; false otherwise
- *
- * @private
- * @function isCommand
- */
-function isCommand(msg) {
-    return (
-        (msg.type === "api") &&
-        msg.content.startsWith(AlchemyUtils.commandPrefix)
-    );
-}
-
-/**
- * Parses the API command out of the given Roll20 chat message
- *
- * @param msg {Object} Roll20 chat message data
- *
- * @returns {string} The name of the command to execute
- *
- * @private
- * @function parseCommand
- */
-function parseCommand(msg) {
-    return msg.content
-        .split(" ")[0]
-        .toLowerCase()
-        .replace(AlchemyUtils.commandPrefix, "");
-}
-
-/**
- * Strips the command out of the chat message and returns the rest of the input parameters as an Array
- *
- * @param msg {Object} Roll20 chat message
- *
- * @returns {String[]} List of input parameters provided in chat message
- *
- * @private
- * @function parseInput
- */
-function parseInput(msg) {
-    // Dumb implementation; will break if e.g. needs to accept text strings with spaces in them
-    return _.tail(msg.content.split(/\s+/));
-}
-
-/**
- * Invokes the given command with the given input on the corresponding route
- *
- * @param command {String} The command to execute
- * @param input {String[]} The raw input parameters to pass to the command
- *
- * @returns {void}
- *
- * @private
- * @function execute
- */
-function execute(command, input) {
+on("ready", () => {
     const routes = {
         "gather": AlchemyGatherer.gather,
         "harvest": AlchemyHarvester.harvest
     };
 
-    if (!(routes[command] && (typeof routes[command] === "function"))) {
-        return;
-    }
+    on("chat:message", ApiRouter(AlchemyUtils.commandPrefix, routes));
 
-    routes[command](...input);
-}
-
-on("ready", () => { log(`[AA] v${version} loaded.`); });
-on("chat:message", route);
+    log(`[AA] v${version} loaded.`);
+});
